23e9cf12a09eb248b4c7af0feea40bab59d2f2ab
julieta sangari <juuli.sangari@gmail.com>
2021-10-28 23:48:49 -0300

    dom

--- java.js
+++ java.js
@@ -1,61 +1,32 @@
 
-// const paquetes = [];
-
-// const paquete1 = {id:1, nombre : 'paquetes en argentina x1', precio:20432}
-// const paquete2 = {id:2, nombre : 'paquetes en europa x2', precio:35000}
-// const paquete3 = {id:3, nombre : 'paquetes en centro america x3', precio:50000}
-
-// paquetes.push(paquete1);
-// paquetes.push(paquete2);
-// paquetes.push(paquete3);
-
-// console.log("cantidad de paquetes" , paquetes.lenght);
-// for(const elemento of paquetes){
-//     console.log('id' , elemento.id);
-//     console.log('precio' , elemento.precio);
-//     console.log('', elemento.nombre);
-
-// }
-
-function precioDeLaCuota(){
-   let precioTotal = 21000;
-   let cuotaSeleccionada = parseFloat(prompt("ingrese en cuantas cuotas desea abonar el viaje (3,6,12)"));
-   let resultado =  precioTotal / cuotaSeleccionada;
-    alert ("Con la cuota seleccionada abonara mensualmente " + resultado);
-
-}
-
-precioDeLaCuota();
-
-
-class paquete{
-    constructor(destino, precio, stockInicial){
-        this.destino = destino;
-        this.precio = parseFloat(precio);
-        this.stock = parseInt(stockInicial);
-   
-    }
-    Vender(cantidad){
-        if(this.verificarStock(cantidad)){
-            this.stock = this.stock - cantidad;
-            console.log("se vendieron "+ cantidad  + this.destino);
-            console.log("El stock actual es: " + this.stock);
-        }
-        else{
-            console.log("No hay suficiente stock para vender a " + this.destino + ". Cantidad diponible para vender: " + this.stock);
-        }
-       
-    }
-    verificarStock(cantidad){
-        if( this.stock >= cantidad){
-            return true;
-        }
+const paquetes = [
+    {id:0, nombre:'Mexico', descripcion:'Viaje a Cancun, aereo ida y vuelta mas all inclusive 7 noches', precio:100000},
+    {id:1, nombre:'Europa', descripcion:'Viaje a Italia Roma hotel 3 estrellas 5 noches aereo ida y vuelta', precio:380000},
+    {id:2, nombre:'EEUU', descripcion: 'Viaje a miami ida y vuelta hotel all inclusive 10 noches', precio:150000},
+    {id:3, nombre:'Argentina', descripcion:'Viaje a Bariloche ida y vuelta traslados incluidos hotel holiday inn 6 noches', precio:85000}
+]
+const mostrarPaquetes = () => {
+    for (const paquete of paquetes){
+        let contenedor = document.createElement("div");
+        contenedor.innerHTML = 
+        `
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">${paquete.nombre}</h5>
+                    <p class="card-text">${paquete.descripcion}</p>
+                    <p class="card-text"><b>$ ${paquete.precio}</b></p>
+                    <a href="#" class="btn btn-primary" onclick=obtenerCantidadProductosComprados(${paquete.id})>Comprar</a>
+                </div>
+            </div>
+            
+               
+        `
+        document.getElementById('fila').appendChild(contenedor);
+      
     }
 }
-const paquete1 = new paquete("viajes con amigos", 1300, 20);
-const paquete2 = new paquete("viajes de aventura", 1600, 30);
-const paquete3 = new paquete("Lugares exoticos", 3000, 5);
-
-paquete1.Vender(7);
-paquete2.Vender(15);
-paquete3.Vender(10);
+const obtenerCantidadPaquetesComprados = () => {
+    var cantidad = parseFloat(document.getElementsByTagName('span')[0].innerHTML);
+    cantidad = cantidad + 1;
+    document.getElementsByTagName('span')[0].innerHTML = cantidad;
+}
