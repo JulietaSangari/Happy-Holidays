369849f63e51e1e0e1a7eb6d4b1b4c1eba381de2
julieta sangari <juuli.sangari@gmail.com>
2021-11-02 20:09:33 -0300

    Agregar eventos

--- java.js
+++ java.js
@@ -1,12 +1,12 @@
 
 const paquetes = [
-    {id:0, nombre:'Mexico', descripcion:'Viaje a Cancun, aereo ida y vuelta mas all inclusive 7 noches', precio:100000},
+    {id:0, nombre:'Mexico', descripcion:'Viaje a Cancun, aereo ida y vuelta mas all inclusive 7 noches', precio:100000 },
     {id:1, nombre:'Europa', descripcion:'Viaje a Italia Roma hotel 3 estrellas 5 noches aereo ida y vuelta', precio:380000},
     {id:2, nombre:'EEUU', descripcion: 'Viaje a miami ida y vuelta hotel all inclusive 10 noches', precio:150000},
     {id:3, nombre:'Argentina', descripcion:'Viaje a Bariloche ida y vuelta traslados incluidos hotel holiday inn 6 noches', precio:85000}
 ]
 const mostrarPaquetes = () => {
-    for (const paquete of paquetes){
+    for (const paquete of paquetesFiltrados){
         let contenedor = document.createElement("div");
         contenedor.innerHTML = 
         `
@@ -18,15 +18,53 @@ const mostrarPaquetes = () => {
                     <a href="#" class="btn btn-primary" onclick=obtenerCantidadPaquetesComprados(${paquete.id})>Comprar</a>
                 </div>
             </div>
-            
                
         `
         document.getElementById('fila').appendChild(contenedor);
       
     }
 }
-const obtenerCantidadPaquetesComprados = () => {
-    var cantidad = parseFloat(document.getElementsByTagName('span')[0].innerHTML);
-    cantidad = cantidad + 1;
-    document.getElementsByTagName('span')[0].innerHTML = cantidad;
+const obtenerCantidadPaquetesComprados = (idPaquete) => {
+    console.log(idPaquete);
+    var elemento = document.getElementsByTagName('span')[0]
+    var cantidad = parseFloat(elemento.innerHTML) + 1;
+    elemento.innerHTML = cantidad;
+    agregarPaqueteAlCarrito(idPaquete);
+}
+
+
+let boton = document.getElementById("btnFiltrar");
+boton.addEventListener("click", filtrarDatos);
+
+let inputBusqueda = document.getElementById("busqueda");
+inputBusqueda.addEventListener("keyup", busquedaPorTeclado);
+
+function busquedaPorTeclado(){
+    console.log(inputBusqueda.value);
+    if (inputBusqueda.value.length > 3){
+        filtrarDatos();
+    }
+}
+
+let paquetesFiltrados = paquetes;
+
+function filtrarDatos(){
+    let palabraClave = document.getElementById("busqueda");
+    paquetesFiltrados = paquetes.filter(elemento => elemento.descripcion.includes(palabraClave.value) || elemento.nombre.includes(palabraClave.value));
+    limpiarHTML();
+    mostrarPaquetes();
+    console.log(paquetesFiltrados);
+
+}
+
+const limpiarHTML = () =>{
+    let fila = document.getElementById('fila');
+    document.body.removeChild(fila);
+
+    
+    fila = document.createElement("div");
+    let a = document.createAttribute("id");
+    a.value='fila';
+    fila.setAttributeNode(a);
+    document.body.appendChild(fila);
 }
