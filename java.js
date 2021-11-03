0479da8cd905714696ae77df4ba83487d3aa1e83
julieta sangari <juuli.sangari@gmail.com>
2021-11-02 22:39:43 -0300

    eventos

--- java.js
+++ java.js
@@ -1,23 +1,27 @@
 
 const paquetes = [
-    {id:0, nombre:'Mexico', descripcion:'Viaje a Cancun, aereo ida y vuelta mas all inclusive 7 noches', precio:100000 },
-    {id:1, nombre:'Europa', descripcion:'Viaje a Italia Roma hotel 3 estrellas 5 noches aereo ida y vuelta', precio:380000},
-    {id:2, nombre:'EEUU', descripcion: 'Viaje a miami ida y vuelta hotel all inclusive 10 noches', precio:150000},
-    {id:3, nombre:'Argentina', descripcion:'Viaje a Bariloche ida y vuelta traslados incluidos hotel holiday inn 6 noches', precio:85000}
+    {id:0, nombre:'Mexico', descripcion:'Viaje a Cancun, aereo ida y vuelta mas all inclusive 7 noches', precio:100000, imagen:src="imagenes/cancun.jpg"},
+    {id:1, nombre:'Europa', descripcion:'Viaje a Italia Roma hotel 3 estrellas 5 noches aereo ida y vuelta', precio:380000, imagen:src="imagenes/roma.jpg"},
+    {id:2, nombre:'EEUU', descripcion: 'Viaje a miami ida y vuelta hotel all inclusive 10 noches', precio:150000, imagen:src="imagenes/miami.jpg"},
+    {id:3, nombre:'Argentina', descripcion:'Viaje a Bariloche ida y vuelta traslados incluidos hotel holiday inn 6 noches', precio:85000, imagen:src="imagenes/bariloche.jpg"}
 ]
 const mostrarPaquetes = () => {
     for (const paquete of paquetesFiltrados){
         let contenedor = document.createElement("div");
         contenedor.innerHTML = 
         `
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">${paquete.nombre}</h5>
-                    <p class="card-text">${paquete.descripcion}</p>
-                    <p class="card-text"><b>$ ${paquete.precio}</b></p>
-                    <a href="#" class="btn btn-primary" onclick=obtenerCantidadPaquetesComprados(${paquete.id})>Comprar</a>
-                </div>
-            </div>
+          <div class="card" style="width: 18rem;">
+           
+                    <img src="${paquete.imagen}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${paquete.nombre}</h5>
+                        <p class="card-text">${paquete.descripcion}</p>
+                        <p class="card-text"><b>$ ${paquete.precio}</b></p>
+                        <a href="#" class="btn btn-primary" onclick=obtenerCantidadPaquetesComprados(${paquete.id})>Comprar</a>
+                   </div>   
+          </div>
+          <br></br>
+            
                
         `
         document.getElementById('fila').appendChild(contenedor);
